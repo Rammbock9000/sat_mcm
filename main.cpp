bca7c79cbe4d08afb074675c0a4ec9a0b69e3478
nfiege <uk025743@student.uni-kassel.de>
2022-12-06 16:41:50 +0100

    can now pass lower limit for the adder count from console

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@ int main(int argc, char** argv) {
 	bool allow_node_output_shift = false;
 	bool write_cnf = false;
 	int allow_coefficient_sign_inversion = 0;
+	int min_num_add = -1;
 #ifdef USE_Z3
 	solver_name = "z3";
 #endif
@@ -42,7 +43,7 @@ int main(int argc, char** argv) {
 	solver_name = "cadical";
 #endif
 	if (argc == 1) {
-		std::cout << "Please call satscm like this: ./satscm <constant(s)> <solver name> <timeout> <threads> <quiet> <minimize full adders> <allow post adder right shfits> <allow negative coefficients> <write cnf files> <allow coefficient sign inversion>" << std::endl;
+		std::cout << "Please call satscm like this: ./satscm <constant(s)> <solver name> <timeout> <threads> <quiet> <minimize full adders> <allow post adder right shfits> <allow negative coefficients> <write cnf files> <allow coefficient sign inversion> <min num add>" << std::endl;
 		std::cout << "  => constant(s): <int:int:...>: colon-separated list of integers that should be computed" << std::endl;
 		std::cout << "  => solver name: <string>: cadical, z3, syrup are supported" << std::endl;
 		std::cout << "  => timeout: <uint>: number of seconds allowed per SAT instance" << std::endl;
@@ -53,6 +54,7 @@ int main(int argc, char** argv) {
 		std::cout << "  => allow negative coefficients: <0/1>: allow the use of negative coefficients to decrease the FA count" << std::endl;
 		std::cout << "  => write cnf files: <0/1>: write all SAT programs to CNF files" << std::endl;
 		std::cout << "  => allow coefficient sign inversion: <0/1/-1>: 1 - allow the SAT solver to invert the sign of ANY requested coefficient to reduce the FA count; -1 - only allow it if for negative requested coefficients; 0 - never allow it" << std::endl;
+		std::cout << "  => min num add: <uint>: lower limit for the adder count (default: number of unique odd coefficients excluding 0 and 1)" << std::endl;
 		return 0;
 	}
 	if (argc > 1) {
@@ -163,6 +165,17 @@ int main(int argc, char** argv) {
 			throw std::runtime_error(err_msg.str());
 		}
 	}
+	if (argc > 11) {
+		std::string s(argv[11]);
+		try {
+			min_num_add = std::stoi(s);
+		}
+		catch (...) {
+			std::stringstream err_msg;
+			err_msg << "failed to convert " << s << " to int" << std::endl;
+			throw std::runtime_error(err_msg.str());
+		}
+	}
 	std::cout << "Starting OSCM for constant" << (C.size()>1?"s\n":" ");
 	for (auto &c : C) {
 		std::cout << (C.size()>1?"  ":"") << c << (C.size()>1?"\n":" ");
@@ -195,6 +208,7 @@ int main(int argc, char** argv) {
 	if (also_minimize_full_adders) solver->also_minimize_full_adders();
 	if (allow_node_output_shift) solver->allow_node_output_shift();
 	if (allow_coefficient_sign_inversion != 0) solver->ignore_sign(allow_coefficient_sign_inversion == -1);
+	if (min_num_add >= 0) solver->set_min_add(min_num_add);
 	solver->solve();
 	auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time).count() / 1000.0;
 	std::cerr << "Finished solving after " << elapsed_time << " seconds" << std::endl;
