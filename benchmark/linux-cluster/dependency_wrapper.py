578e0a2befdbd84894aea5b0edd93b0ac2d7d6d0
nfiege <uk025743@student.uni-kassel.de>
2024-04-26 17:14:44 +0200

    is it working?!?!?!?!?

--- benchmark/linux-cluster/dependency_wrapper.py
+++ benchmark/linux-cluster/dependency_wrapper.py
@@ -4,8 +4,6 @@ import subprocess
 def create_cmm_slurm_script(experiment):
     time_limit = f"6-00:00:00"
     num_tasks = 48
-    time_limit = f"0-00:01:00"
-    num_tasks = 2
     mem = 5000*num_tasks
     base_dir="/home/groups/fb16-digi/projects/constmult/sat_mcm/"
     script_base="benchmark/scripts"
@@ -47,8 +45,6 @@ def create_cleanup_slurm_script(experiment):
         f.write(f"#SBATCH -p pub23\n")
         f.write(f"#SBATCH --mem=4000\n")
         f.write(f"#SBATCH --time=0-00:10:00\n")
-        f.write(f"#SBATCH --mail-user=nfiege@uni-kassel.de\n")
-        f.write(f"#SBATCH --mail-type=ALL\n")
         f.write(f"#SBATCH --output={base_dir}benchmark/linux-cluster/clean-{experiment}.out\n")
         f.write(f"cd {base_dir};python3 {script_base}/cleanup_intermediate.py {result_dir} {experiment}\n")
     return filename
@@ -59,9 +55,7 @@ def start_job_and_get_id(job, dep=None):
         command = ["sbatch", f"{job}"]
     else:
         command = ["sbatch", f"--dependency=afterany:{dep}", job]
-    print(f"PYTHON: start executing command: {command}")
     output = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
-    print(f"PYTHON: finished")
     elems = output.split()
     if len(elems) < 4:
         raise Exception(f"failed to queue batch jobs (code 0 - '{output}')")
@@ -81,12 +75,12 @@ def main():
         clean_file = create_cleanup_slurm_script(experiment)
         job_id = start_job_and_get_id(clean_file)
         job_id = start_job_and_get_id(cmm_file, dep=job_id)
-        #job_id = start_job_and_get_id(clean_file, dep=job_id)
-        #job_id = start_job_and_get_id(cmm_file, dep=job_id)
-        #job_id = start_job_and_get_id(clean_file, dep=job_id)
-        #job_id = start_job_and_get_id(cmm_file, dep=job_id)
-        #job_id = start_job_and_get_id(clean_file, dep=job_id)
-        #job_id = start_job_and_get_id(cmm_file, dep=job_id)
+        job_id = start_job_and_get_id(clean_file, dep=job_id)
+        job_id = start_job_and_get_id(cmm_file, dep=job_id)
+        job_id = start_job_and_get_id(clean_file, dep=job_id)
+        job_id = start_job_and_get_id(cmm_file, dep=job_id)
+        job_id = start_job_and_get_id(clean_file, dep=job_id)
+        job_id = start_job_and_get_id(cmm_file, dep=job_id)
         job_id = start_job_and_get_id(clean_file, dep=job_id)
         job_id = start_job_and_get_id(cmm_file, dep=job_id)
         _ = start_job_and_get_id(clean_file, dep=job_id)
