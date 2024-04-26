4829b09db531446cf9e0910d22e8ae21e57b7b6f
nfiege <uk025743@student.uni-kassel.de>
2024-04-26 12:18:44 +0200

    debugging, 3rd try

--- benchmark/linux-cluster/dependency_wrapper.py
+++ benchmark/linux-cluster/dependency_wrapper.py
@@ -1,23 +1,75 @@
 import subprocess
 
 
+def create_cmm_slurm_script(experiment):
+    time_limit = f"6-00:00:00"
+    num_tasks = 48
+    time_limit = f"0-00:01:00"
+    num_tasks = 2
+    base_dir="/home/groups/fb16-digi/projects/constmult/sat_mcm/"
+    script_base="benchmark/scripts"
+    filename = f"{base_dir}benchmark/linux-cluster/start_cmm-{experiment}.sh"
+    with open(filename, "w") as f:
+        f.write(f"#!/bin/bash\n")
+        f.write(f"#SBATCH --nodes=1\n")
+        f.write(f"#SBATCH --tasks-per-node={num_tasks}\n")
+        f.write(f"#SBATCH -p public2023\n")
+        f.write(f"#SBATCH --mem=251000\n")
+        f.write(f"#SBATCH --time={time_limit}\n")
+        f.write(f"#SBATCH --mail-user=nfiege@uni-kassel.de\n")
+        f.write(f"#SBATCH --mail-type=ALL\n")
+        f.write(f"#SBATCH --output={base_dir}benchmark/linux-cluster/cmm-{experiment}.out\n")
+        if experiment == "cmm":
+            f.write(f"cd {base_dir};python3 {script_base}/cmm_benchmark.py {num_tasks-1}\n")
+        elif experiment == "pcmm":
+            f.write(f"cd {base_dir};python3 {script_base}/pcmm_benchmark.py {num_tasks-1}\n")
+        elif experiment == "complex":
+            f.write(f"cd {base_dir};python3 {script_base}/complex_mult_benchmark.py {num_tasks-1}\n")
+        elif experiment == "pcomplex":
+            f.write(f"cd {base_dir};python3 {script_base}/pcomplex_mult_benchmark.py {num_tasks-1}\n")
+        elif experiment == "conv":
+            f.write(f"cd {base_dir};python3 {script_base}/conv_core_benchmark.py {num_tasks-1}\n")
+        elif experiment == "pconv":
+            f.write(f"cd {base_dir};python3 {script_base}/pconv_core_benchmark.py {num_tasks-1}\n")
+    return filename
+
+
+def create_cleanup_slurm_script(experiment):
+    base_dir="/home/groups/fb16-digi/projects/constmult/sat_mcm/"
+    script_base="benchmark/linux-cluster"
+    filename = f"{base_dir}{script_base}/start_cleanup-{experiment}.sh"
+    result_dir=f"{base_dir}benchmark/results/cmm_rnd"
+    with open(filename, "w") as f:
+        f.write(f"#!/bin/bash\n")
+        f.write(f"#SBATCH --nodes=1\n")
+        f.write(f"#SBATCH --tasks-per-node=1\n")
+        f.write(f"#SBATCH -p public2023\n")
+        f.write(f"#SBATCH --mem=4000\n")
+        f.write(f"#SBATCH --time=0-00:10:00\n")
+        f.write(f"#SBATCH --mail-user=nfiege@uni-kassel.de\n")
+        f.write(f"#SBATCH --mail-type=ALL\n")
+        f.write(f"#SBATCH --output={base_dir}benchmark/linux-cluster/clean-{experiment}.out\n")
+        f.write(f"cd ${base_dir};python3 {script_base}/cleanup_intermediate.py {result_dir} {experiment}\n")
+    return filename
+
+
 def start_job_and_get_id(job, dep=None):
     if dep is None:
         command = ["sbatch", f'"{job}"']
     else:
-        command = ["sbatch", f"'--dependency=afterany:{dep} {job}'"]
+        command = ["sbatch", f'"--dependency=afterany:{dep} {job}"']
     output = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
     elems = output.split()
     if len(elems) < 5:
-        raise Exception("failed to queue batch jobs (code 0)")
+        raise Exception(f"failed to queue batch jobs (code 0 - '{output}')")
     if elems[0] != "successfully":
-        raise Exception("failed to queue batch jobs (code 1)")
+        raise Exception(f"failed to queue batch jobs (code 1 - '{elems[0]}')")
     if elems[1] != "submitted":
-        raise Exception("failed to queue batch jobs (code 2)")
+        raise Exception(f"failed to queue batch jobs (code 2 - '{elems[1]}')")
     if elems[2] != "batch":
-        raise Exception("failed to queue batch jobs (code 3)")
+        raise Exception(f"failed to queue batch jobs (code 3 - '{elems[2]}')")
     if elems[3] != "job":
-        raise Exception("failed to queue batch jobs (code 4)")
+        raise Exception(f"failed to queue batch jobs (code 4 - '{elems[3]}')")
     return elems[4]
 
 
@@ -25,14 +77,16 @@ def main():
     experiments = ["cmm", "pcmm", "conv", "pconv", "complex", "pcomplex"]
     experiments = ["cmm"]
     for experiment in experiments:
-        job_id_1 = start_job_and_get_id(f"start_cmm.sh {experiment}")
-        job_id_2 = start_job_and_get_id(f"start_cleanup.sh {experiment}", dep=job_id_1)
-        job_id_3 = start_job_and_get_id(f"start_cmm.sh {experiment}", dep=job_id_2)
-        #job_id_4 = start_job_and_get_id(f"start_cleanup.sh {experiment}", dep=job_id_3)
-        #job_id_5 = start_job_and_get_id(f"start_cmm.sh {experiment}", dep=job_id_4)
-        #job_id_6 = start_job_and_get_id(f"start_cleanup.sh {experiment}", dep=job_id_5)
-        #job_id_7 = start_job_and_get_id(f"start_cmm.sh {experiment}", dep=job_id_6)
-        #_ = start_job_and_get_id(f"start_cleanup.sh {experiment}", dep=job_id_7)
+        cmm_file = create_cmm_slurm_script(experiment)
+        clean_file = create_cleanup_slurm_script(experiment)
+        job_id_1 = start_job_and_get_id(cmm_file)
+        job_id_2 = start_job_and_get_id(clean_file, dep=job_id_1)
+        job_id_3 = start_job_and_get_id(cmm_file, dep=job_id_2)
+        #job_id_4 = start_job_and_get_id(clean_file, dep=job_id_3)
+        #job_id_5 = start_job_and_get_id(cmm_file, dep=job_id_4)
+        #job_id_6 = start_job_and_get_id(clean_file, dep=job_id_5)
+        #job_id_7 = start_job_and_get_id(cmm_file, dep=job_id_6)
+        #_ = start_job_and_get_id(clean_file, dep=job_id_7)
 
 
 if __name__ == '__main__':
